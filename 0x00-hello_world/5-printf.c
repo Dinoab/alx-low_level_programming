2ec794d271913ac882779c2e747c3b0fe5483847
dino abdela delil <104862943+Dinoab@users.noreply.github.com>
2022-06-03 06:24:16 -0700

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - Prints "with proper grammar,
- *                 but the outcome is a piece of art.
- *
- * Return: Always 0.
- */
+ * main - prints string
+ * Return: Always 0 (Success)
+ **/
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
