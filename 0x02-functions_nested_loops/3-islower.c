45701d7d92feb22d18454d1a2488b8247d165987
dino abdela delil <104862943+Dinoab@users.noreply.github.com>
2022-06-08 07:28:54 -0700

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
- * _islower - returns 1 if lowercase alphabet and 0 otherwise
- * @c: takes in a character
- * Return: 0 for uppercase, 1 for lowercase
- */
-
+* _islower - returns 1 if lowercase alphabet and 0 otherwise
+* @c: takes in a character
+* Return: 0 for uppercase, 1 for lowercase
+*/
 int _islower(int c)
 
 {
