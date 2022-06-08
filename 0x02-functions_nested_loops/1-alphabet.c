1cfffaff471c03115f21299016251fc00e1afb71
Dinoab <delildin4@gmail.com>
2022-06-08 02:10:33 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+include "main.h"
+
+/**
+ *
+ *  * print_alphabet - prints alphabet in lowercase
+ *
+ *   * @void: no argument
+ *
+ *    **/
+
+void print_alphabet(void)
+
+{
+
+		char c;
+
+
+
+			for (c = 'a'; c <= 'z'; c++)
+
+						_putchar(c);
+
+				_putchar('\n');
+
+}
