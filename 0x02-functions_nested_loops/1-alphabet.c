4d79b9545dba65cbba92fce7861893bc08137eee
dino abdela delil <104862943+Dinoab@users.noreply.github.com>
2022-06-08 03:37:55 -0700

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -11,15 +11,12 @@ void print_alphabet(void)
 		int j;
 
 
-			for (j = 'a'; j <= 'z'; j++)
+		for (j = 'a'; j <= 'z'; j++)
 
-					{
+		{
+			_putchar(j);
 
-							_putchar(j);
-
-								}
-
-
-				_putchar('\n');
+		}
+	        _putchar('\n');
 
 }
