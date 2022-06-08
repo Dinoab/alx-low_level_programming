dad85c7181e06e7e3f03bbe990e91abf57bfbfaa
dino abdela delil <104862943+Dinoab@users.noreply.github.com>
2022-06-08 03:54:01 -0700

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
- * print_alphabet_10x - prints alphabet in lowercase
- * @void: no argument
- **/
-
+ * print_alphabet_x10 - prints the alphabet in lowercase 10 times
+ * @void - no argument
+ */
 void print_alphabet_x10(void)
 
 {
