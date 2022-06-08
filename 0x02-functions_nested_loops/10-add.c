a833612c1c9f85e2eff1d3bbc640018cd7da755a
dino abdela delil <104862943+Dinoab@users.noreply.github.com>
2022-06-08 18:18:28 +0300

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,13 @@
 #include "main.h"
 /**
- * add - function that adds two integers together
- * @a: takes in an integer
- * @b: takes in an integer
- * Return: a + b result
- */
+*add - function that adds two integers together
+* @a: takes in an integers
+* @b: takes in an integers
+*Return: a + b result
+*/
 int add(int a, int b)
+
 {
-	return (a + b);
+    return (a + b);
+    
 }
