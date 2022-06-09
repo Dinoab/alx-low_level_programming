3b59de9c8afc87c49a78ed42efcab87485b644d1
dino abdela delil <104862943+Dinoab@users.noreply.github.com>
2022-06-09 01:21:35 -0700

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  *
  * *main-tests function that prints if integer is positive or negative
@@ -7,21 +6,12 @@
  * *Return:0
  *
  * */
-
 int main(void)
 
 {
-
 	int i;
-
-
-
+	
 	i=0;
-
 	positive_or_negative(i);
-
-
-
 	return (0);
-
 }
