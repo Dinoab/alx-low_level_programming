32145e1307f09595c34e8a26df8450e0a0d07340
Dinoab <delildin4@gmail.com>
2022-06-09 11:26:45 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,10 +8,16 @@ void print_line(int n)
 {
 	int x;
 
-	if (n > 0)
+	if (n <= 0)
+	{
+		_putchar ('\n');
+	}
+	else
 	{
 		for (x = 0; x < n; x++)
-			_putchar ('_');
+		{
+			_putchar (95);
+		}
 		_putchar ('\n');
 	}
 }
