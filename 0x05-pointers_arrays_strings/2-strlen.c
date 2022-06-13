5038c13d45fe0719fdac1bcb9d64428dc6789a69
Dinoab <delildin4@gmail.com>
2022-06-13 11:51:11 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_strlen - return the length of a string
+ *@s: character
+ *Return: length os string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	;
+	}
+	return (i);
+
+}
+}
+
