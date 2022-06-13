2e8037dfe6fc1a1ca502d1cbb5993b880fe51878
Dinoab <delildin4@gmail.com>
2022-06-13 11:31:25 -0700

    pointers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,25 +2,19 @@
 #include <time.h>
 #include <stdlib.h>
 /**
-<<<<<<< HEAD
- * * main - entry block
- * * @void: no argument
- * * Return: 0 (Sucess)
- * **/
-=======
-* main - entry block
-* @void: no argument
-* Return: 0 (Sucess)
-**/
+ * main - entry block
+ * @void: no argument
+ * Return: 0 (Sucess)
+ */
 
 int main(void)
-
+	
 {
 	int n;
-	
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
+
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
@@ -29,3 +23,9 @@ int main(void)
 	{
 		printf("%d is zero\n", n);
 	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
