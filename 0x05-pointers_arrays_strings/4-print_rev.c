3e5691054dda2a40f5fd7fbdeb0e805823d4f623
dino abdela delil <104862943+Dinoab@users.noreply.github.com>
2022-06-13 13:07:57 -0700

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
  * print_rev - print in reverse a string
- * @s:takes a string of characters
+ * @str:takes a string of characters
  */
-void print_rev(char *s)
+void print_rev(char *str)
 {
 	int i = 0, c;
 
-	while (s[c] != '\0')
+	while (str[c] != '\0')
 		c++;
 	c--;
 	for (i = c; i >= 0; i--)
-		_putchar(s[i]);
+		_putchar(str[i]);
 	_putchar('\n');
 }
