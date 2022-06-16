4400c55e8087bf9fcfada77b28e612616109fbb2
dino abdela delil <104862943+Dinoab@users.noreply.github.com>
2022-06-16 02:52:30 -0700

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,15 +1,18 @@
 #include "main.h"
-#include <string.h>
 /**
  * _strcmp - compares two strings
- * @s1: param1
- * @s2: param2
- * Return: interger
+ * @s1: string 1 to be compared
+ * @s2: string 2 to be compared
+ * Return: negativve if s1<s2, 0 if matching and positive int if s1 > s2
  */
 int _strcmp(char *s1, char *s2)
 {
-	int result;
+	int i;
 
-	result = strcmp( s1,s2)
-return (result);
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+return (0);
 }
