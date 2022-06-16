73e28b14ae38942e71a61cdb9668033fe0dbf524
Dinoab <delildin4@gmail.com>
2022-06-16 01:18:23 -0700

    pointers

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strncpy - copy a strin
+ * @dest: destination string
+ * @src: source of string
+ * @n: max byte of src
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j;
+
+	while (src[i] != '\0')
+		i++;
+	for (j = 0; j < n && src[j] != '\0'; j++)
+		dest[j] = src[j];
+	for (; n > i; i++)
+}
