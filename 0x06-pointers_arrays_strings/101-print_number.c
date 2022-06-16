ba26a7115686e70832acedd56df819f45fb140dd
dino abdela delil <104862943+Dinoab@users.noreply.github.com>
2022-06-16 05:29:30 -0700

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,11 +3,11 @@
 /**
  * print_number: prints an intiger
  * @n: number to be printed
- * Return: always 0 (sucess)
+ * Return: Always 0 (Sucess)
  */
 void print_number (int n)
 {
-	int power , neg , hold;
+	int power, neg, hold;
 
 	neg = 0;
 	power = 1;
@@ -15,32 +15,30 @@ void print_number (int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		neg=1;
+		neg = 1;
 	}
 	while (hold > 9 || hold <-9)
 	{
-		power *=10;
-		hold /=10;
+		power *= 10;
+		hold /= 10;
 	}
 	while (power > 0)
 	{
 		if (power > 9)
 		{
 			if (!neg)
-				_putchar((n % 10) = '0');
+				_putchar((n /power % 10) + '0');
 			else
-				_putchar((n % 10) * -1 = '0');
+				_putchar((n / power % 10) * -1 + '0');
 			power /= 10;
 		}
 		if (power == 1)
 		{
 			if (neg)
-				_putchar((n % 10) * -1 = '0');
+				_putchar((n % 10) * -1 + '0');
 			else
-				 _putchar((n % 10) = '0');
+				 _putchar((n % 10) + '0');
 		power = 0;
 		}
 	}
 }
-
-
