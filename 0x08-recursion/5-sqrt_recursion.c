ca1c2593e496486b327698c606b8e05f0217d292
Dinoab <delildin4@gmail.com>
2022-06-22 13:38:51 -0700

    recurssion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,6 +5,7 @@
  * Return: value squeare
  */
 int _sqrt_recursion(int n)
+int find_sqrt(int num, int root);
 {
 	if (a * a > b)
 		return (-1);
@@ -12,4 +13,19 @@ int _sqrt_recursion(int n)
 		return (a);
 	else
 		return (_sqrt2(a += 1, b));
+}
+/**
+ * find_sqrt - Finds the natural square root of an inputted number.
+ * @num: The number to find the square root of.
+ * @root: The root to be tested.
+ * Return: If the number has a natural square root - the square root.
+ * If the number does not have a natural square root - -1.
+ */
+int find_sqrt(int num, int root)
+{
+	if ((root * root) == num)
+		return (root);
+	if (root == num / 2)
+		return (-1);
+	return (find_sqrt(num, root + 1));
 }
