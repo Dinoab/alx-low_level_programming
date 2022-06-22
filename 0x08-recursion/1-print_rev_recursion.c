902437159587ea70000c05955597a9444b13d701
Dinoab <delildin4@gmail.com>
2022-06-22 13:18:04 -0700

    recurssion more

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string of characters
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_print_rev_recursion(s + 1);
+	}
+}
---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - square root
+ * @n: integer
+ * Return: value squeare
+ */
+int _sqrt_recursion(int n)
+{
+	if (a * a > b)
+		return (-1);
+	else if (a * a == b)
+		return (a);
+	else
+		return (_sqrt2(a += 1, b));
+}
