61e1414039b1e6d3edbf04e38939ec02a0220c69
dino abdela delil <104862943+Dinoab@users.noreply.github.com>
2022-06-23 01:47:39 -0700

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,7 +4,7 @@ int _sqrt(int, int);
 /**
  * _sqrt_recursion - sqrt using recursion.
  * @n: parameter to be square root of.
- * Return: If n has a natural square root of a number.
+ * Return: square root of a number.
  */
 int _sqrt_recursion(int n);
 {
