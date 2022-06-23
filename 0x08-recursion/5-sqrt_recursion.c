551d2e96ea8592e233e529f0fad72808176294d6
dino abdela delil <104862943+Dinoab@users.noreply.github.com>
2022-06-23 02:14:24 -0700

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,28 +1,43 @@
 #include "main.h"
 
-int _sqrt(int, int);
+int find_sqrt(int num, int root);
 int _sqrt_recursion(int n);
+
 /**
- * _sqrt_recursion - sqrt using recursion.
- * @n: parameter to be square root of.
- * Return: square root of a number.
+ * find_sqrt - Finds the natural square root of an inputted number.
+ * @num: The number to find the square root of.
+ * @root: The root to be tested.
+ *
+ * Return: If the number has a natural square root - the square root.
+ *         If the number does not have a natural square root - -1.
+ */
+int find_sqrt(int num, int root)
+{
+	if ((root * root) == num)
+		return (root);
+
+	if (root == num / 2)
+		return (-1);
+
+	return (find_sqrt(num, root + 1));
+}
+
+/**
+ * _sqrt_recursion - Returns the natural square root of a number.
+ * @n: The number to return the square root of.
+ *
+ * Return: If n has a natural square root - the natural square root of n.
+ *         If n does not have a natural square root - -1.
  */
 int _sqrt_recursion(int n)
 {
-	return (_sqrt(n, 1));
-}
-/**
- * _sqrt - recursive square root.
- * @n: number.
- * @i: iterator.
- * Return: a number.
- */
-int _sqrt(int n, int i)
-	int square = i * i;
+	int root = 0;
 
-	if (square > n)
+	if (n < 0)
 		return (-1);
-	if (square == n)
-		return (i);
-	return (_sqrt(n, i + 1));
+
+	if (n == 1)
+		return (1);
+
+	return (find_sqrt(n, root));
 }
