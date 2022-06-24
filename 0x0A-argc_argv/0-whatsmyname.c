04a359230365e585c0ca838d98f8267d0ad5f167
dino abdela delil <104862943+Dinoab@users.noreply.github.com>
2022-06-24 12:13:52 -0700

    Update 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry
+ * @argc: string
+ * @argv: array
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int val;
+
+	for (val = 0; val < argc; val++)
+		printf("%s\n", argv[val]);
+	return (0);
+}
