0db60ada1de1777b5801e755113f4d18440e1450
dino abdela delil <104862943+Dinoab@users.noreply.github.com>
2022-07-06 09:57:43 -0700

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,36 +1,32 @@
 #include "3-calc.h"
-#include <stdlib.h>
-#include <stdio.h>
 /**
  * main - Prints the result of simple operations.
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
  * Return: Always 0.
  */
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int num1, num2;
-	char *op;
+	int (*op_func)(int, int), a, b;
 
 	if (argc != 4)
 	{
-		printf("Error\n");
+		printf("Error\n"),
 		exit(98);
 	}
-	num1 = atoi(argv[1]);
-	op = argv[2];
-	num2 = atoi(argv[3]);
-	if (get_op_func(op) == NULL || op[1] != '\0')
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	op_func = get_op_func(argv[2]);
+	if (!op_func)
 	{
-		printf("Error\n");
+		printf("Error\n"),
 		exit(99);
 	}
-	if ((*op == '/' && num2 == 0) ||
-			(*op == '%' && num2 == 0))
+	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
 	{
-		printf("Error\n");
+		printf("Error\n"),
 		exit(100);
 	}
-	printf("%d\n", get_op_func(op)(num1, num2));
+	printf("%d\n", op_func(a, b));
 	return (0);
 }
