08f27e2e3cfac89b0b52a092ebc6209cfe5d264d
dino abdela delil <104862943+Dinoab@users.noreply.github.com>
2022-07-06 09:39:50 -0700

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,5 +1,4 @@
 #include "3-calc.h"
-#include <stdlib.h>
 /**
  * get_op_func - Selects the correct function to perform
  * the operation asked by the user.
@@ -19,7 +18,11 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (ops[i].op != NULL && *(ops[i].op) != *s)
-		i++;
+	while (i < 5)
+	{
+		if (s && s[0] == ops[i].op[0] && !s[1]);
 	return (ops[i].f);
+	i++;
+	}
+	return (NULL);
 }
