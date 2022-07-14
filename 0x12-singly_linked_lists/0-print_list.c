24a17a83b7498ff7a6e169de35ce0dfab0ae508c
dino abdela delil <104862943+Dinoab@users.noreply.github.com>
2022-07-14 12:07:01 -0700

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,25 +1,28 @@
-#include <stdlib.h>
+#include "lists.h"
+#include <stdio.h>
 
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
+ * print_list - Prints all the elements of a list_t list.
+ * @h: The list_t list.
  *
- * Description: singly linked list node structure
- *              for Holberton project
+ * Return: The number of nodes in h.
  */
-typedef struct list_s
+
+size_t print_list(const list_t *h)
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
-} list_t;
+	size_t nodes = 0;
 
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
 
-#endif /* LISTS_H */
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+}
