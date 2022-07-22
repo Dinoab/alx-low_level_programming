f8c71861de261aac61d88f740d835d3d86a2d37d
dino abdela delil <104862943+Dinoab@users.noreply.github.com>
2022-07-22 06:14:30 -0700

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,28 +1,33 @@
 #include "main.h"
 
 /**
- * binary_to_uint - function that converts binary to unsigned int
- * @b: takes in a conts char string
- * Return: the converted unsigned int
+ * binary_to_uint - Entry Point
+ * @b: const char
+ * Return: 0
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int con = 0, dec = 0;
+	unsigned int res = 0;
+	int base = 1, i = 0;
 
 	if (b == NULL)
 		return (0);
 
-	while (b[con] != '\0')
+	while (b[i + 1])
 	{
-		if (b[con] != '0' && b[con] != '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-
-		dec <<= 1;
-
-		if (b[con] == '1')
-			dec ^= 1;
-		con++;
+		i++;
 	}
 
-	return (dec);
+	while (i >= 0)
+	{
+		res += ((b[i] - '0') * base);
+		base *= 2;
+		i--;
+	}
+
+
+	return (res);
+
 }
