f344414cc1c7fe96829ccdfd8c846d73f9f3afae
dino abdela delil <104862943+Dinoab@users.noreply.github.com>
2022-09-05 08:08:54 -0700

    Update 8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -31,7 +31,6 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		if (tmp->next != NULL)
 			tmp->next->prev = tmp->prev;
 	}       
-	        free(tmp);
-		return (1);
+	free(tmp);
+	return (1);
 }
-
